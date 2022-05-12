4a6a51c1d59c9973873d24da53ea821726fbcbee
Mahmoudi <www.oussamaa@outlook.fr>
2022-05-12 10:59:57 +0000

    first commit

---
+++ src/test/java/ma/atos/banking/management/BankingManagement/BankingManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package ma.atos.banking.management.BankingManagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankingManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
